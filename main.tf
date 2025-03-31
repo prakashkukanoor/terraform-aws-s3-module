d8aabb4329e0d361dea288428d37306ac8b5218d
prakashkukanoor <prakashkop054@gmail.com>
2025-03-31 17:30:29 -0400

    s3 module

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ locals {
 # Create s3 bucket
 resource "aws_s3_bucket" "this" {
   count = length(var.bucket_names)
+  region = var.region
 
   bucket = var.bucket_names[count.index] # Replace with your bucket name
 
