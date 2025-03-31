795ff478a2dc7c323572f3fa14e1d309ade8ac53
prakashkukanoor <prakashkop054@gmail.com>
2025-03-31 16:54:25 -0400

    s3 module

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
-output "vpc" {
-  value = aws_vpc.this.id
-}
+# output "vpc" {
+#   value = aws_vpc.this.id
+# }
 
-output "application_public_subnets" {
-  value = aws_subnet.application_public[*].id
-}
+# output "application_public_subnets" {
+#   value = aws_subnet.application_public[*].id
+# }
 
-output "application_private_subnets" {
-  value = aws_subnet.application_private[*].id
-}
+# output "application_private_subnets" {
+#   value = aws_subnet.application_private[*].id
+# }
 
-output "database_private_subnets" {
-  value = aws_subnet.database_private[*].id
-}
+# output "database_private_subnets" {
+#   value = aws_subnet.database_private[*].id
+# }
