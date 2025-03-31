e12c4efbff79d838dbd198054022ec050ae96c07
prakashkukanoor <prakashkop054@gmail.com>
2025-03-31 16:18:27 -0400

    s3 module

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
-variable "bucket_name" {
+variable "bucket_names" {
   description = "Name for the S3 bucket"
-  type        = string
+  type        = list(string)
 }
 
 variable "path_to_json_file" {
@@ -13,4 +13,27 @@ variable "environment" {
 }
 variable "team" {
   type = string
+}
+
+variable "block_public_acls" {
+  type    = bool
+  default = true
+
+}
+
+variable "block_public_policy" {
+  type    = bool
+  default = true
+
+}
+
+variable "ignore_public_acls" {
+  type    = bool
+  default = true
+
+}
+variable "restrict_public_buckets" {
+  type    = bool
+  default = true
+
 }
