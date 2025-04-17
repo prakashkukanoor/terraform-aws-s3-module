9a84bc3f4c52f47b138ff0f2231e5e14411a3097
prakashkukanoor <prakashkop054@gmail.com>
2025-04-17 09:03:21 -0400

    s3 module to support multiple teams

--- main.tf
+++ main.tf
@@ -28,16 +28,17 @@ resource "aws_s3_bucket" "this" {
 }
 
 # Block public access settings
-# resource "aws_s3_bucket_public_access_block" "this" {
-#   for_each = toset(var.bucket_names)
+# Block public access settings
+resource "aws_s3_bucket_public_access_block" "this" {
+  for_each = toset([for obj in aws_s3_bucket.this: obj.bucket])
 
-#   bucket = aws_s3_bucket.this[each.key].id
+  bucket = aws_s3_bucket.this[each.key].id
 
-#   block_public_acls       = var.block_public_acls
-#   block_public_policy     = var.block_public_policy
-#   ignore_public_acls      = var.ignore_public_acls
-#   restrict_public_buckets = var.restrict_public_buckets
-# }
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
 
 # # Enable versioning
 # resource "aws_s3_bucket_versioning" "this" {
