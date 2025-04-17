1d5a20e3d2f260e445a9ee60d9e4b90ddc76bbe3
prakashkukanoor <prakashkop054@gmail.com>
2025-04-17 09:09:09 -0400

    s3 module to support multiple teams

--- main.tf
+++ main.tf
@@ -20,14 +20,12 @@ locals {
 resource "aws_s3_bucket" "this" {
   for_each = {for idx, value in local.applications_data: "${value.bucket_name}" => value }
   
-  
   bucket = each.value.bucket_name
   tags = merge(
     local.common_tags,
   { team = "${each.value.team}" })
 }
 
-# Block public access settings
 # Block public access settings
 resource "aws_s3_bucket_public_access_block" "this" {
   for_each = toset([for obj in aws_s3_bucket.this: obj.bucket])
@@ -40,37 +38,37 @@ resource "aws_s3_bucket_public_access_block" "this" {
   restrict_public_buckets = var.restrict_public_buckets
 }
 
-# # Enable versioning
-# resource "aws_s3_bucket_versioning" "this" {
-#   for_each = toset(var.bucket_names)
+# Enable versioning
+resource "aws_s3_bucket_versioning" "this" {
+  for_each = toset([for obj in aws_s3_bucket.this: obj.bucket])
 
-#   bucket = aws_s3_bucket.this[each.key].id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+  bucket = aws_s3_bucket.this[each.key].id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# # Server-side encryption configuration
-# resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-#   for_each = toset(var.bucket_names)
+# Server-side encryption configuration
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  for_each = toset([for obj in aws_s3_bucket.this: obj.bucket])
 
-#   bucket = aws_s3_bucket.this[each.key].id
+  bucket = aws_s3_bucket.this[each.key].id
 
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
 # Attach iam policy to the s3
 # resource "aws_s3_bucket_policy" "this" {
-#   for_each = toset(var.bucket_names)
+#   for_each = {for idx, value in local.applications_data: "${value.bucket_name}" => value }
 
-#   bucket = aws_s3_bucket.this[each.key].id
+#   bucket = aws_s3_bucket.this[each.value.bucket_name].id
 
-#   policy = jsondecode(templatefile("${var.path_to_json_file}", {
-#     bucket_id = aws_s3_bucket.this[count.index].id
+#   policy = jsondecode(templatefile("${each.value.policy_json_tpl_file_path}", {
+#     bucket_id = aws_s3_bucket.this["${each.value.bucket_name}"].id
 #   }))
 
 # }
