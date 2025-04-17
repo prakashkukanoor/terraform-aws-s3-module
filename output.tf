4a4afc79eaa2b3fec25431a5ad44ab44265bcf14
prakashkukanoor <prakashkop054@gmail.com>
2025-04-17 10:46:57 -0400

    s3 module to support multiple teams

--- output.tf
+++ output.tf
@@ -3,14 +3,6 @@ output "buckets" {
   value = [for s3_obj in aws_s3_bucket.this : s3_obj.bucket]
 }
 
-# output "application_public_subnets" {
-#   value = aws_subnet.application_public[*].id
-# }
-
-# output "application_private_subnets" {
-#   value = aws_subnet.application_private[*].id
-# }
-
-# output "database_private_subnets" {
-#   value = aws_subnet.database_private[*].id
-# }
+output "flattened" {
+  value = local.applications_data
+}
