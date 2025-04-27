3e3a8ac26be6b1323d16655b2424efd29e6c4107
prakashkukanoor <prakashkop054@gmail.com>
2025-04-27 18:16:13 -0400

    root module setup

--- variables.tf
+++ variables.tf
@@ -3,19 +3,28 @@ variable "applications" {
     buckets                   = list(string)
     policy_json_tpl_file_path = string
   }))
+  default = {
+    "devops-domain" = {
+      buckets = ["devops-test"]
+      policy_json_tpl_file_path = ""
+    }
+  }
 }
 
 variable "region" {
   type        = string
   description = "region for bucket creation"
+  default = "us-east-1"
 }
 
 
 variable "environment" {
   type = string
+  default = "DEV"
 }
 variable "team" {
   type = string
+  default = "devops"
 }
 
 variable "block_public_acls" {
